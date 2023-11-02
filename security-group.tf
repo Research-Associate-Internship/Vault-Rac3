849c21bdc7b80533d57c4cb9d93a24fd4687246d
ashwindh <109006799+ashwindh@users.noreply.github.com>
2023-11-02 15:02:35 -0400

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "vault-rac3-SG" {
  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["192.168.0.28/32"]
   }
 
